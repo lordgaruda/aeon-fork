6464092b151ef807af0306b3684e9aed43ae8755
lordgaruda <falgunpatel.fv@gmail.com>
2024-07-17 23:26:14 +0530

    aeon-fork:  image support

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -21,7 +21,7 @@ async def mirror_status(_, message):
         msg += f"\n<b>• Bot uptime</b>: {currentTime}"
         msg += f"\n<b>• Free disk space</b>: {free}"
 
-        reply_message = await sendMessage(message, msg)
+        reply_message = await sendMessage(message, msg, photo='Random')
         await deleteMessage(message)
         await one_minute_del(reply_message)
     else:
