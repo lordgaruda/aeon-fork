b3f36255175645e0d7256ffc885b421b0e2ee45c
lordgaruda <falgunpatel.fv@gmail.com>
2024-07-18 11:33:23 +0530

    aeon-fork: Refector and minor fix. - **Enhancements**:     - Refactored `torrent_select.py` to improve code readability and maintainability by splitting large functions into smaller ones.     - Removed unused imports and redundant code from multiple files to streamline the codebase.     - Simplified string formatting in several files for consistency and readability. - **CI**:     - Added a new GitHub Actions workflow (`ruff.yml`) to automatically format code using Ruff on push to the `dev` branch.

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -25,10 +25,11 @@ async def speedtest(_, message):
         await editMessage(speed, "Speedtest failed to complete.")
         return
     
-    string_speed  = f"<b>SPEEDTEST INFO</b>\n\n"
+    string_speed  = "<b>SPEEDTEST INFO</b>\n\n"
     string_speed += f"<b>• Ping:</b> <code>{result.ping} ms</code>\n"
     string_speed += f"<b>• Upload:</b> <code>{get_readable_file_size(result.upload / 8)}/s</code>\n"
     string_speed += f"<b>• Download:</b> <code>{get_readable_file_size(result.download / 8)}/s</code>\n"
+    string_speed += f"<b>• IP Address:</b> <code>{result.client['ip']}</code>"
 
     try:
         await sendMessage(message, string_speed, photo=result.share())
